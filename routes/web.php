63366c5a121b3eb574d7d81f44fb20144f758418
Mireya599 <cdelacruzb1@miumg.edu.gt>
2024-11-01 16:35:10 -0600

    implementacion de nuevos paquetes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\ProveedoreController;
+use App\Http\Controllers\CompraController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +14,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('categorias', App\Http\Controllers\CategoriaController::class);
+Route::resource('productos', App\Http\Controllers\ProductoController::class);
+Route::resource('proveedores', App\Http\Controllers\ProveedoreController::class);
+Route::resource('compras', App\Http\Controllers\CompraController::class);
