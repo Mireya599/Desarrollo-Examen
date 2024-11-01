e51c4481cf39bd5dea94829fd581d0376e5431e1
Mireya599 <cdelacruzb1@miumg.edu.gt>
2024-11-01 16:37:44 -0600

    Creacion de toda la tabla y controlador de categorias

---
+++ app/Models/Categoria.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class Categoria
+ *
+ * @property $id
+ * @property $nombre
+ * @property $descripcion
+ * @property $created_at
+ * @property $updated_at
+ * @property $deleted_at
+ *
+ * @property Producto[] $productos
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Categoria extends Model
+{
+    use SoftDeletes;
+
+    protected $perPage = 20;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['nombre', 'descripcion'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function productos()
+    {
+        return $this->hasMany(\App\Models\Producto::class, 'id', 'categorias_id');
+    }
+    
+}
